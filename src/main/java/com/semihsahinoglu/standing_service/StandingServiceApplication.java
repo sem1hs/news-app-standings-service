c0a6129cfba3426c5c65e313bd330c627d48106d
sem1hs <semihgtt11@gmail.com>
2026-01-26 22:52:50 +0300

    dosyalar eklendi

---
+++ src/main/java/com/semihsahinoglu/standing_service/StandingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.standing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandingServiceApplication.class, args);
+	}
+
+}
